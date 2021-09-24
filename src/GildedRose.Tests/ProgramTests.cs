5c57823eb955beaf32228ae6c88c8175e7b3fe13
Christina Braun <11138584+chrstnbrn@users.noreply.github.com>
2021-09-24 23:14:13 +0200

    migrate to .net 6

---
+++ src/GildedRose.Tests/ProgramTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace GildedRose.Tests
+{
+    public class ProgramTests
+    {
+        [Fact]
+        public void TestTheTruth()
+        {
+            Assert.True(true);
+        }
+    }
+}
